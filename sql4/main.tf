3a3b40c4285ccb592c7f96d276695dc4508bc65a
matthiasSC-BA <55184044+matthiasSC-BA@users.noreply.github.com>
2021-04-27 18:28:41 +0200

    Update main.tf

--- sql4/main.tf
+++ sql4/main.tf
@@ -1,9 +1,14 @@
 module "sql-db" {
   source  = "GoogleCloudPlatform/sql-db/google//modules/mysql"
   version = "4.0.0"
-  name = "testdbgfk2"
+  name  = var.name
   database_version = "MYSQL_5_7"
   project_id = "t-vra-gfk-terraform"
   zone = "c"
   region = "europe-west3"
+  deletion_protection = false
+  
+  random_instance_name = true
+  
+
 }
